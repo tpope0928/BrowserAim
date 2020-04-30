cac11a716acfa9472f94c7bec485de53895b63c8
Thomas_Pope <tpope0928@gmail.com>
2020-04-30 17:43:09 -0400

    backend complete for now. figureout how to conenct to FE

--- backend/app/serializers/user_serializer.rb
+++ backend/app/serializers/user_serializer.rb
@@ -1,3 +1,12 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username
+
+  def games
+    self.object.games.map do |game|
+      {score: game.score,
+       accuracy: game.accuracy,
+       headshot_percent: game.headshot_percent}
+    end
+
+  end
 end
