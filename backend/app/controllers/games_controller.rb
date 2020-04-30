f4bc469145f098c51e82da5d3c761b3cf8e5d541
Thomas_Pope <tpope0928@gmail.com>
2020-04-30 17:28:41 -0400

    games and user controller

--- backend/app/controllers/games_controller.rb
+++ backend/app/controllers/games_controller.rb
@@ -1,2 +1,11 @@
 class GamesController < ApplicationController
+  def index
+    @games = Game.all
+    render json: @games
+  end
+
+  def create
+    @user = User.find_by(username: params[:username])
+    Game.create(user_id: @user.id, score: params[:score], accuracy: params[:accuracy], headshot_percent: params[:headshot_percent])
+  end
 end
