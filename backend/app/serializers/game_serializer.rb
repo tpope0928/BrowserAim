ababe1a02c8394876208b3e374bfe9ec9d0b0c84
Thomas_Pope <tpope0928@gmail.com>
2020-05-07 17:59:25 -0400

    serializer refactor

--- backend/app/serializers/game_serializer.rb
+++ backend/app/serializers/game_serializer.rb
@@ -1,3 +1,5 @@
 class GameSerializer < ActiveModel::Serializer
   attributes :id, :score, :accuracy, :user_id, :headshot_percent
+
+  belongs_to :user
 end
--- backend/app/serializers/user_serializer.rb
+++ backend/app/serializers/user_serializer.rb
@@ -1,6 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username
 
+  has_many :games
+
   def games
     self.object.games.map do |game|
       {score: game.score,
